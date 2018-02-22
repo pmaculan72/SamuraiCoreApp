da3a66df6802351d4bde11b5e78685d7875c9130
PAULO-DEV <paulo.maculan@gmail.com>
2018-02-22 18:03:05 -0300

    Add project files.

---
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Domain;
+
+namespace SamuraiApp.Data
+{
+    public class SamuraiContext : DbContext
+    {
+        public SamuraiContext(DbContextOptions<SamuraiContext> options) : base(options) { }
+
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+        public DbSet<Battle> Battles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SamuraiBattle>()
+                .HasKey(s => new { s.SamuraiId, s.BattleId });
+        }
+    } 
+}
